457b6ae1ca3705e12b619ed4a9f8978b79de5d76
hotire <gngh0101@gmail.com>
2021-03-02 00:32:13 +0900

    Implement Jaccard

--- src/main/java/com/github/hotire/spring/similarity/SimilarityService.java
+++ src/main/java/com/github/hotire/spring/similarity/SimilarityService.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 
 import org.apache.commons.text.similarity.HammingDistance;
 import org.apache.commons.text.similarity.IntersectionSimilarity;
+import org.apache.commons.text.similarity.JaccardSimilarity;
 
 public interface SimilarityService {
     HammingDistance HAMMING_DISTANCE = new HammingDistance();
     IntersectionSimilarity<String> INTERSECTION_SIMILARITY = new IntersectionSimilarity<>(charSequence -> Arrays.asList(charSequence.toString().split("")));
+
+    JaccardSimilarity JACCARD_SIMILARITY = new JaccardSimilarity();
 
     default Integer hammingDistance(final CharSequence left, final CharSequence right) {
         return HAMMING_DISTANCE.apply(left, right);
@@ -15,6 +18,10 @@ public interface SimilarityService {
 
     default Integer intersection(final CharSequence left, final CharSequence right) {
         return INTERSECTION_SIMILARITY.apply(left, right).getIntersection();
+    }
+
+    default Double intersectionRate(final CharSequence left, final CharSequence right) {
+        return JACCARD_SIMILARITY.apply(left, right);
     }
 
 }
