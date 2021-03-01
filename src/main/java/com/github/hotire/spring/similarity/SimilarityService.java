f781390a48d47529c520d403b487b12d4e68525c
hotire <gngh0101@gmail.com>
2021-03-01 19:07:10 +0900

    Add INTERSECTION_SIMILARITY

--- src/main/java/com/github/hotire/spring/similarity/SimilarityService.java
+++ src/main/java/com/github/hotire/spring/similarity/SimilarityService.java
@@ -1,9 +1,13 @@
 package com.github.hotire.spring.similarity;
 
+import java.util.Arrays;
+
 import org.apache.commons.text.similarity.HammingDistance;
+import org.apache.commons.text.similarity.IntersectionSimilarity;
 
 public interface SimilarityService {
     HammingDistance HAMMING_DISTANCE = new HammingDistance();
+    IntersectionSimilarity<String> INTERSECTION_SIMILARITY = new IntersectionSimilarity<>(charSequence -> Arrays.asList(charSequence.toString().split("")));
 
     default Integer hammingDistance(final CharSequence left, final CharSequence right) {
         return HAMMING_DISTANCE.apply(left, right);
