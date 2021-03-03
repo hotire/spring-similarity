62009ead5c034b5710e896c47e3df9987b8b2502
hotire <gngh0101@gmail.com>
2021-03-04 00:20:34 +0900

    Use utils instead of instance in simhash

---
+++ src/main/java/com/github/hotire/spring/similarity/SimHashUtils.java
@@ -0,0 +1,70 @@
+package com.github.hotire.spring.similarity;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class SimHashUtils {
+    private static final int hashbits = 32;
+
+    private static final int chunkSize = 4;
+    private static final int overlapSize = 3;
+
+    public static String doFilter(String source) {
+        return source.replaceAll("[.!,?)\"']+|[！，。？、~…）]+", "$0 ")
+                     .trim()
+                     .replaceAll("\\s+",  " ");
+    }
+
+    public static List<String> tokens(String source) {
+        List<String> binaryWords = new LinkedList<>();
+        final String[] splitInput = source.split(" ");
+        for (int position = 0; position < (splitInput.length - chunkSize + 1); position += chunkSize - overlapSize ) {
+            final StringJoiner joiner = new StringJoiner(" ");
+            for (int i = 0; i < chunkSize; i++) {
+                if (position + i < splitInput.length) {
+                    joiner.add(splitInput[position+i]);
+                }
+            }
+            binaryWords.add(joiner.toString());
+        }
+        return binaryWords;
+    }
+
+    public static long hash32(String str) {
+        int sum = 0;
+        for (byte b : str.getBytes()) {
+            sum = sum * 31 + (Byte.toUnsignedInt(b));
+        }
+        return Integer.toUnsignedLong(sum);
+    }
+
+    public static long simhash32(String source) {
+        final int[] bits = new int[hashbits];
+        final List<String> tokens = tokens(source);
+        for (String t : tokens) {
+            long v = hash32(t);
+            for (int i = hashbits; i >= 1; --i) {
+                if (((v >> (hashbits - i)) & 1) == 1) {
+                    ++bits[i - 1];
+                } else {
+                    --bits[i - 1];
+                }
+            }
+        }
+
+        long hash = 0x0000000000000000L;
+        long one = 0x0000000000000001L;
+        for (int i = hashbits; i >= 1; --i) {
+            if (bits[i - 1] > 1) {
+                hash |= one;
+            }
+            one = one << 1;
+        }
+        return hash;
+    }
+
+    public static String simhash32String(String source) {
+        return String.format("%08x", simhash32(source));
+    }
+}
